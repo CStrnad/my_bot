808b638555ccc046958e0c02a8f2f8ba97740265
CStrnad <cstrnad2@illinois.edu>
2023-10-09 22:11:01 -0500

    actually disabled gazebo this time

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -26,22 +26,22 @@ def generate_launch_description():
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #          )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-    #                     arguments=['-topic', 'robot_description',
-    #                                '-entity', 'my_bot'],
-    #                     output='screen')
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
 
 
 
     # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
+        # gazebo,
         spawn_entity,
     ])
